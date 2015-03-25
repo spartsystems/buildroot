23ae35bbde177add3afafa461e59a39669cc15c7
Philippe Normand <philn@igalia.com>
2015-03-25 10:49:32 +0100

    libnice: bump to 0.1.10

--- package/libnice/libnice.mk
+++ package/libnice/libnice.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-LIBNICE_VERSION = 0.1.7
+LIBNICE_VERSION = 0.1.10
 LIBNICE_SOURCE = libnice-$(LIBNICE_VERSION).tar.gz
 LIBNICE_SITE = http://nice.freedesktop.org/releases/
 LIBNICE_INSTALL_STAGING = YES
 LIBNICE_LICENSE = MPL 1.1 and LGPL 2.1
 LIBNICE_LICENSE_FILES = COPYING.MPL COPYING.LGPL
 
-LIBNICE_AUTORECONF = YES
+LIBNICE_DEPENDENCIES = gstreamer1
 
 $(eval $(autotools-package))
